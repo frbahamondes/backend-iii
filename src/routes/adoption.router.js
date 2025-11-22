2eadc32c3b51f7894d87dcbcf31da4364d70d3af
Francisca Bahamondes <frbahamondes@gmail.com>
2025-11-22 19:01:47 -0300

    Funcion DELETE en adoption controller

--- src/routes/adoption.router.js
+++ src/routes/adoption.router.js
@@ -1,10 +1,11 @@
-import { Router} from 'express';
+import { Router } from 'express';
 import adoptionsController from '../controllers/adoptions.controller.js';
 
 const router = Router();
 
-router.get('/',adoptionsController.getAllAdoptions);
-router.get('/:aid',adoptionsController.getAdoption);
-router.post('/:uid/:pid',adoptionsController.createAdoption);
+router.get('/', adoptionsController.getAllAdoptions);
+router.get('/:aid', adoptionsController.getAdoption);
+router.post('/:uid/:pid', adoptionsController.createAdoption);
+router.delete('/:aid', adoptionsController.deleteAdoption); // ✅ Nuevo endpoint
 
 export default router;
